ec0bf8279ee9cb060cc93f1c5b2e3dd28670125d
DESKTOP-BVONRVL\BRUNO <brunoacc92@gmail.com>
2021-05-02 17:27:40 +0100

    extended crew member roles enum

--- src/main/java/com/springboot/training/spaceover/spacecrew/manager/enums/SpaceCrewMemberRole.java
+++ src/main/java/com/springboot/training/spaceover/spacecrew/manager/enums/SpaceCrewMemberRole.java
@@ -2,6 +2,6 @@ package com.springboot.training.spaceover.spacecrew.manager.enums;
 
 public enum SpaceCrewMemberRole {
 
-    PILOT, CAPTAIN, TECHNICIAN
+    COMMANDER_OFFICER, ENGINEER_OFFICER, SCIENCE_OFFICER, PILOT_OFFICER, SUPPORT_OFFICER
 
 }
